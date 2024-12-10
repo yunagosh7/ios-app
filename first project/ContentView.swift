03f1923e58ae0ddef690a6258fe0b34b3fa15d24
Juan Cruz Vila <parlantephilips7@gmail.com>
2024-12-10 16:28:29 -0300

    feature: finish card

--- first project/ContentView.swift
+++ first project/ContentView.swift
@@ -9,14 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                Image("images")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack(alignment: .top) {
+                    
+                    Text("Iguazu's Waterfalls")
+                        .font(.system(size: 24, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, 16)
+                    Spacer()
+                    VStack {
+                        
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                            Image(systemName: "star")
+                            
+                        }.padding(EdgeInsets(top: 4, leading: 0, bottom: 0, trailing: 0))
+                            .foregroundColor(.orange)
+                        Text("(234 reviews)")
+                            .foregroundColor(.orange)
+                            .font(.system(size: 12))
+                    }
+                    
+                }
+                
+                
+                Text("Come visit the falls, an experience of a lifetime")
+                    .font(.system(size: 20))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                
+                }
+                .foregroundColor(.gray)
+            }
+            .padding()
+            .background(Rectangle().foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+            )
+            .padding()
         }
-        .padding()
+        
     }
+        
 }
 
 #Preview {
